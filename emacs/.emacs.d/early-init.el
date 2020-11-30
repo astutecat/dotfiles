71e5aa4640b21a4c00f860366bf131cfb83e085e
Will Rogers <email@willrogers.me.uk>
2020-11-30 13:18:01 +0100

    emacs tweak, add zshrc

--- emacs/.emacs.d/early-init.el
+++ emacs/.emacs.d/early-init.el
@@ -4,7 +4,7 @@
 
 (add-to-list 'default-frame-alist '(height . 50))
 (add-to-list 'default-frame-alist '(width . 100))
-(add-to-list 'default-frame-alist '(background-color . "black"))
+(add-to-list 'default-frame-alist '(background-color . "#282a36"))
 (add-to-list 'default-frame-alist '(foreground-color . "white"))
 
 (when (version<= "26.0.50" emacs-version )
