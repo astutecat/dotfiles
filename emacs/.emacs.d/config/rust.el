21cad2513cb2074db8a59f97135f084889a39c71
Will R <email@astutecat.dev>
2021-01-19 17:06:38 +0100

    rust and completion changes

--- emacs/.emacs.d/config/rust.el
+++ emacs/.emacs.d/config/rust.el
@@ -1,4 +1,9 @@
-(add-hook 'rust-mode-hook #'racer-mode)
-;; (add-hook 'racer-mode-hook #'eldoc-mode)
+(with-eval-after-load 'company (add-to-list 'company-backends 'company-racer))
 
+(add-hook 'rust-mode-hook #'racer-mode)
+(add-hook 'racer-mode-hook #'eldoc-mode)
 (add-hook 'racer-mode-hook #'company-mode)
+
+(setq company-tooltip-align-annotations t)
+(setq racer-rust-src-path
+  "/Users/astutecat/.rustup/toolchains/stable-x86_64-apple-darwin/lib/rustlib/src/rust/library")
