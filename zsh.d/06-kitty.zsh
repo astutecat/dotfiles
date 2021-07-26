8031b79c0f75183937ebe47e3988c043ce996e7d
Will Rogers <email@willrogers.me.uk>
2021-07-26 09:21:04 +0200

    add ssh alias if TERM is xterm-kitty

--- zsh.d/06-kitty.zsh
+++ zsh.d/06-kitty.zsh
@@ -14,3 +14,7 @@ fi
 
 mkdir -p ~/.config/kitty
 [[ -h ~/.config/kitty/kitty.conf ]] || ln -sf ~/.kitty.conf ~/.config/kitty/kitty.conf
+
+if [[ $TERM == 'xterm-kitty' ]]; then
+  alias ssh="kitty +kitten ssh"
+fi
