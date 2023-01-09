b95dbd7ddf65e1f820fedeff76c76246785f091f
Will R <github@astutecat.dev>
2023-01-09 10:13:16 +0100

    cmp tweak

--- private_dot_config/nvim/lua/plugins/config/cmp.lua
+++ private_dot_config/nvim/lua/plugins/config/cmp.lua
@@ -1,38 +1,12 @@
-local cmp = require'cmp'
+local cmp = require 'cmp'
 local lspkind = require('lspkind')
 
-local lspkind_comparator = function(conf)
-  local lsp_types = require('cmp.types').lsp
-  return function(entry1, entry2)
-    if entry1.source.name ~= 'nvim_lsp' then
-      if entry2.source.name == 'nvim_lsp' then
-        return false
-      else
-        return nil
-      end
-    end
-    local kind1 = lsp_types.CompletionItemKind[entry1:get_kind()]
-    local kind2 = lsp_types.CompletionItemKind[entry2:get_kind()]
-
-    local priority1 = conf.kind_priority[kind1] or 0
-    local priority2 = conf.kind_priority[kind2] or 0
-    if priority1 == priority2 then
-      return nil
-    end
-    return priority2 < priority1
-  end
-end
-
-local label_comparator = function(entry1, entry2)
-  return entry1.completion_item.label < entry2.completion_item.label
-end
-
 cmp.setup({
-    completion = {
-      keyword_length = 2
-    },
-    formatting = {
-      format = lspkind.cmp_format({
+  completion = {
+    keyword_length = 2
+  },
+  formatting = {
+    format = lspkind.cmp_format({
       mode = 'symbol', -- do not show text alongside icons
       menu = ({
         buffer = "[buffer]",
@@ -67,65 +41,64 @@ cmp.setup({
         Event = "",
         Operator = "",
         TypeParameter = ""
-    },
-        maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
-
-        -- The function below will be called before any actual modifications from lspkind
-        -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
-        before = function (entry, vim_item)
-          vim_item.dup = ({
-            vsnip = 0,
-            nvim_lsp = 0
-          })[entry.source.name] or 1
-          return vim_item
-        end
-      })
-    },
-    snippet = {
-      expand = function(args)
-        vim.fn["vsnip#anonymous"](args.body)
-      end,
-    },
-    mapping = cmp.mapping.preset.insert({
-      ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-      ['<C-f>'] = cmp.mapping.scroll_docs(4),
-      ['<CR>'] = cmp.mapping.confirm({ select = false }),
-      }),
-    sources = {
-        { name = 'vsnip', keyword_length=3, priority = 6 },
-        { name = 'nvim_lsp', priority = 8 },
-        { name = 'buffer', max_item_count = 5, priority = 7},
-        { name = 'tags', keyword_length = 3, max_item_count = 5, priority = 6 },
-        { name = 'latex_symbols', priority = 5, max_item_count = 5 }
       },
-      sorting = {
-        comparators = {
-          cmp.config.compare.offset,
-          cmp.config.compare.exact,
-          cmp.config.compare.score,
-          cmp.config.compare.recently_used,
-          cmp.config.compare.locality,
-          cmp.config.compare.kind,
-          cmp.config.compare.sort_text,
-          cmp.config.compare.length,
-          cmp.config.compare.order,
-        },
-      },
-    })
+      maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
 
-    cmp.setup.cmdline('/', {
-      mapping = cmp.mapping.preset.cmdline(),
-      sources = {
-        { name = 'buffer' }
-      }
+      -- The function below will be called before any actual modifications from lspkind
+      -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
+      before = function(entry, vim_item)
+        vim_item.dup = ({
+          vsnip = 0,
+          nvim_lsp = 0
+        })[entry.source.name] or 1
+        return vim_item
+      end
     })
+  },
+  snippet = {
+    expand = function(args)
+      vim.fn["vsnip#anonymous"](args.body)
+    end,
+  },
+  mapping = cmp.mapping.preset.insert({
+    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<CR>'] = cmp.mapping.confirm({ select = false }),
+  }),
+  sources = {
+    { name = 'vsnip', keyword_length = 2, priority = 6 },
+    { name = 'nvim_lsp', priority = 8 },
+    { name = 'buffer', max_item_count = 5, priority = 7 },
+    { name = 'tags', keyword_length = 3, max_item_count = 5, priority = 6 },
+    { name = 'latex_symbols', keyword_length = 2, priority = 5, max_item_count = 5 }
+  },
+  sorting = {
+    comparators = {
+      cmp.config.compare.offset,
+      cmp.config.compare.exact,
+      cmp.config.compare.score,
+      cmp.config.compare.recently_used,
+      cmp.config.compare.locality,
+      cmp.config.compare.kind,
+      cmp.config.compare.sort_text,
+      cmp.config.compare.length,
+      cmp.config.compare.order,
+    },
+  },
+})
 
-    cmp.setup.cmdline(':', {
-      mapping = cmp.mapping.preset.cmdline(),
-      sources = cmp.config.sources({
-        { name = 'path' }
-      }, {
-        { name = 'cmdline' }
-      })
-    })
+cmp.setup.cmdline('/', {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = {
+    { name = 'buffer' }
+  }
+})
 
+cmp.setup.cmdline(':', {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = cmp.config.sources({
+    { name = 'path' }
+  }, {
+    { name = 'cmdline' }
+  })
+})
