1e34a241815875222adb2ddac8c5a013bf26382c
Will R <github@astutecat.dev>
2023-01-10 16:37:10 +0100

    fix lion

--- private_dot_config/nvim/lua/plugins/config/lion.lua
+++ private_dot_config/nvim/lua/plugins/config/lion.lua
@@ -1,5 +1,4 @@
 vim.g.lion_create_maps    = 1
 vim.g.lion_squeeze_spaces = 1
-vim.g.lion_map_right      = 'ga'
-vim.g.lion_map_left       = 'gA'
-
+vim.g.lion_map_right = 'ga'
+vim.g.lion_map_left = 'gA'
