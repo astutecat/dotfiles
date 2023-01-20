d567a8afb731fe8e39d289c50f6d43683fd28077
Will R <github@astutecat.dev>
2023-01-20 12:09:06 +0100

    minor tweak

--- run_after_install_asdf_plugins.sh
+++ run_after_install_asdf_plugins.sh
@@ -1,13 +1,13 @@
 #!/bin/zsh
 [[ -n $(command -v asdf) ]] || return;
 
-if [[ ! -n $(command -v adr) ]]; then
+if [[ -z $(command -v adr) ]]; then
   asdf plugin add adr-tools
   asdf install adr-tools latest
   asdf global adr-tools latest
 fi
 
-if [[ ! -n $(command -v sops) ]]; then
+if [[ -z $(command -v sops) ]]; then
   asdf plugin add sops
   asdf install sops latest
   asdf global sops latest
