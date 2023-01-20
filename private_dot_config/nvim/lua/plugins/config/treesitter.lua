50f01ae8334ce59f3e61ded7d027567ff6de1868
Will R <github@astutecat.dev>
2023-01-20 20:31:10 +0100

    treesitter elang

--- private_dot_config/nvim/lua/plugins/config/treesitter.lua
+++ private_dot_config/nvim/lua/plugins/config/treesitter.lua
@@ -25,8 +25,8 @@ require'nvim-treesitter.configs'.setup {
 
   highlight = {
     enable = true,
-    additional_vim_regex_highlighting = { "erlang", "markdown" },
-    disable = { "latex", "erlang" },
+    additional_vim_regex_highlighting = { "markdown" },
+    disable = { "latex" },
   },
  -- indent = {
  --   enable = true,
