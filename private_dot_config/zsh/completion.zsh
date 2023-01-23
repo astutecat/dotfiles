1954c08337f461a163b5885692c7cc1e570ccb77
Will R <github@astutecat.dev>
2023-01-23 15:13:52 +0100

    add just completions

--- private_dot_config/zsh/completion.zsh
+++ private_dot_config/zsh/completion.zsh
@@ -6,6 +6,10 @@ if is_cmd rustup; then
   rustup completions zsh >! "${ZDOTDIR:-$HOME}/.zfunc/_rustup"
 fi
 
+if is_cmd just; then
+  just --completions zsh >! "${ZDOTDIR:-$HOME}/.zfunc/_just"
+fi
+
 autoload -Uz compinit bashcompinit
 compinit
 bashcompinit
