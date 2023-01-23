cf3c389cb94c7ba4f66104ebc211a96e78b2bff8
Will R <github@astutecat.dev>
2023-01-23 14:40:42 +0100

    add just to asdf auto install

--- run_after_install_asdf_plugins.sh
+++ run_after_install_asdf_plugins.sh
@@ -12,3 +12,9 @@ if [[ -z $(command -v sops) ]]; then
   asdf install sops latest
   asdf global sops latest
 fi
+
+if [[ -z $(command -v just) ]]; then
+  asdf plugin add just
+  asdf install just latest
+  asdf global just latest
+fi
