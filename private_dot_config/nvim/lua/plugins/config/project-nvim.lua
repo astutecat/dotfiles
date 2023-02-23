18972105229fc0a4da0ada71e420b801920a1fd4
Will R <github@astutecat.dev>
2023-02-23 09:56:37 +0100

    add justfile to project indicators

--- private_dot_config/nvim/lua/plugins/config/project-nvim.lua
+++ private_dot_config/nvim/lua/plugins/config/project-nvim.lua
@@ -1,6 +1,17 @@
-require("project_nvim").setup{
-patterns = { ".git", "_darcs", ".hg", ".bzr", ".svn", "Makefile", "package.json", ".tex.latexmain" },
-manual_mode = true,
+require("project_nvim").setup {
+    patterns = {
+        ".git",
+        "_darcs",
+        ".hg",
+        ".bzr", 
+    ".svn", 
+"Makefile", 
+"package.json", 
+".tex.latexmain", 
+"justfile", 
+"Justfile" 
+},
+    manual_mode = true,
 }
 
 require("telescope").load_extension('projects')
