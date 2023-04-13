6bf8983e58f20d4a4c0aea3a1ad8065df1009856
Willam Rogers <william.rogers@entelios.com>
2023-04-13 16:20:34 +0200

    treesitter

--- private_dot_config/nvim/lua/plugins/config/treesitter.lua
+++ private_dot_config/nvim/lua/plugins/config/treesitter.lua
@@ -1,36 +1,39 @@
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = {
-    "nix",
-    "python",
-    "bash",
-    "elixir",
-    "erlang",
-    "lua",
-    "rust",
-    "yaml",
-    "css",
-    "html",
-    "json",
-    "vim",
-    "hcl",
-    "toml",
-    "make",
-    "regex",
-    "ocaml",
-    "r",
-    "cpp",
-    "javascript",
-    "julia",
-    "zig"
-  },
+  -- ensure_installed = {
+  --   "nix",
+  --   "python",
+  --   "bash",
+  --   "elixir",
+  --   "erlang",
+  --   "lua",
+  --   "rust",
+  --   "yaml",
+  --   "css",
+  --   "html",
+  --   "json",
+  --   "vim",
+  --   "hcl",
+  --   "toml",
+  --   "make",
+  --   "regex",
+  --   "ocaml",
+  --   "r",
+  --   "cpp",
+  --   "javascript",
+  --   "julia",
+  --   "zig",
+  --   "markdown"
+  -- },
+  --
+  ensure_installed = "all",
 
   highlight = {
     enable = true,
     additional_vim_regex_highlighting = { "markdown", "erlang" },
     disable = { "latex" },
   },
- -- indent = {
- --   enable = true,
- -- },
+ indent = {
+   enable = true,
+ },
 }
 
