895304e724f860b3f9ff36ceb3a3bb6be8a420a6
Will R <github@astutecat.dev>
2023-04-17 15:38:34 +0200

    feat(neovim): move projectionist

--- private_dot_config/nvim/lua/lazy-plugins/coding.lua
+++ private_dot_config/nvim/lua/lazy-plugins/coding.lua
@@ -5,34 +5,6 @@ return {
         'tpope/vim-sleuth',
         dependencies = { 'sheerun/vim-polyglot' }
     },
-    {
-        'tpope/vim-projectionist',
-        config = function(...)
-            local heuristics = {}
-
-            heuristics["lib/*.ex"] = {
-                alternate = "test/{}_test.exs",
-                type = "source",
-                template = {
-                    "defmodule {camelcase|capitalize|dot} do",
-                    "end"
-                }
-            }
-
-            heuristics["test/*_test.exs"] = {
-                alternate = "lib/{}.ex",
-                type = "test",
-                template = {
-                    "defmodule {camelcase|capitalize|dot}Test do",
-                    "  use ExUnit.Case, async: true",
-                    "",
-                    "  alias {camelcase|capitalize|dot}",
-                    "end"
-                }
-            }
-            vim.g.projectionist_heuristics = heuristics
-        end
-    },
     { 'christoomey/vim-sort-motion' },
     { 'ludovicchabant/vim-gutentags' },
     {
