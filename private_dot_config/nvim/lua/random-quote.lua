266ded501202a537e193b4569bebf3227f988f81
Will R <github@astutecat.dev>
2023-05-11 08:04:17 +0200

    feat(zsh): some more quotes

--- private_dot_config/nvim/lua/random-quote.lua
+++ private_dot_config/nvim/lua/random-quote.lua
@@ -1,7 +1,10 @@
-local M = {}
-
-M[1] = [[
-We do this not because it is easy, but because we thought it would be easy.
-  - The Programmer's Credo]]
+local M = {
+[[We do this not because it is easy, but because we thought it would be easy.
+  - The Programmer's Credo]],
+[[A distributed system is one where the failure of a machine you've never heard of stops you from getting any work done.
+- Leslie Lamport]],
+[[Hope is not a strategy.
+- Site reliability engineers' motto.]]
+}
 
 return M[math.random(#M)]
