7b0e2199a39a3b622615363a64fe811b92b9ffcc
Will R <github@astutecat.dev>
2023-05-11 08:13:03 +0200

    feat(zsh): some more quote-related stuff

--- private_dot_config/nvim/lua/random-quote.lua
+++ private_dot_config/nvim/lua/random-quote.lua
@@ -1,10 +1,15 @@
 local M = {
-[[We do this not because it is easy, but because we thought it would be easy.
+[[We do this not because it is easy,
+but because we thought it would be easy.
   - The Programmer's Credo]],
-[[A distributed system is one where the failure of a machine you've never heard of stops you from getting any work done.
-- Leslie Lamport]],
+[[A distributed system is one where the failure of a machine
+you've never heard of stops you from getting any work done.
+  - Leslie Lamport]],
 [[Hope is not a strategy.
-- Site reliability engineers' motto.]]
+  - Site reliability engineers' motto.]],
+[[If it's a good idea, go ahead and do it.
+It's much easier to apologize than it is to get permission.
+  - Grace Hopper]]
 }
 
 return M[math.random(#M)]
