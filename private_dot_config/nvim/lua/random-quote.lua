8f37feace365777d099c8d0073673825e77fa341
Will R <github@astutecat.dev>
2023-05-11 08:24:26 +0200

    feat(nvim): some more quotes

--- private_dot_config/nvim/lua/random-quote.lua
+++ private_dot_config/nvim/lua/random-quote.lua
@@ -1,15 +1,24 @@
 local M = {
-[[We do this not because it is easy,
-but because we thought it would be easy.
+[[We do this not because it is easy, but because we thought it would be easy.
   - The Programmer's Credo]],
-[[A distributed system is one where the failure of a machine
-you've never heard of stops you from getting any work done.
+[[A distributed system is one where the failure of a machine you've never heard
+of stops you from getting any work done.
   - Leslie Lamport]],
 [[Hope is not a strategy.
   - Site reliability engineers' motto.]],
-[[If it's a good idea, go ahead and do it.
-It's much easier to apologize than it is to get permission.
-  - Grace Hopper]]
+[[If it's a good idea, go ahead and do it. It's much easier to apologize than
+it is to get permission.
+  - Grace Hopper]],
+[[It's not DNS
+There's no way it's DNS
+It was DNS
+  - SSBroski]],
+[[       A COMPUTER
+CAN NEVER BE HELD ACCOUNTABLE
+
+THEREFORE A COMPUTER MUST NEVER
+  MAKE A MANAGEMENT DECISION
+  - IBM Presentation Slide, ca. 1970s]]
 }
 
 return M[math.random(#M)]
