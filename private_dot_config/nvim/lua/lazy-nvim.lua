0a413433558bf898faa731bd9a0e7ac2fc3b3716
Willam Rogers <william.rogers@entelios.com>
2023-05-17 16:51:38 +0200

    chore: tweak lazy.nvim settings

--- private_dot_config/nvim/lua/lazy-nvim.lua
+++ private_dot_config/nvim/lua/lazy-nvim.lua
@@ -5,7 +5,7 @@ if not vim.loop.fs_stat(lazypath) then
     "clone",
     "--filter=blob:none",
     "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",     -- latest stable release
+    "--branch=stable", -- latest stable release
     lazypath,
   })
 end
@@ -19,14 +19,14 @@ local opts = {
   checker = {
     -- automatically check for plugin updates
     enabled = true,
-    concurrency = 8, ---@type number? set to 1 to check for updates very slowly
-    notify = false,       -- get a notification when new updates are found
-    frequency = 3600,     -- check for updates every hour
+    concurrency = 12, ---@type number? set to 1 to check for updates very slowly
+    notify = false,   -- get a notification when new updates are found
+    frequency = 3600, -- check for updates every hour
   },
   change_detection = {
     -- automatically check for config file changes and reload the ui
     enabled = true,
-    notify = false,     -- get a notification when changes are found
+    notify = false, -- get a notification when changes are found
   },
 }
 
