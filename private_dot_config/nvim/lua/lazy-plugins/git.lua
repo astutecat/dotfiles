44f857c844372f016b9e494eea964c84fd905f27
Will R <github@astutecat.dev>
2023-05-22 11:25:59 +0200

    feat(nvim): lazygit keymap

--- private_dot_config/nvim/lua/lazy-plugins/git.lua
+++ private_dot_config/nvim/lua/lazy-plugins/git.lua
@@ -5,6 +5,18 @@ return {
     dependencies = {
       "nvim-lua/plenary.nvim",
     },
+    opts = {},
+    config = function(_, _)
+      local mappings = {
+        {
+          "<leader>gg",
+          "<cmd>LazyGit<cr>",
+          mode = { 'n' },
+          description = "LazyGit"
+        },
+      }
+      require('legendary').keymaps(mappings)
+    end
   },
 
   {
