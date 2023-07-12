94cde8f79d71fd39e563c17aca774bc176d70fbe
Will R <github@astutecat.dev>
2023-07-12 20:15:23 +0200

    chore: null-ls addition

--- private_dot_config/nvim/lua/lazy-plugins/lsp.lua
+++ private_dot_config/nvim/lua/lazy-plugins/lsp.lua
@@ -75,7 +75,8 @@ return {
           null_ls.builtins.formatting.black,
           null_ls.builtins.diagnostics.pylint,
           null_ls.builtins.diagnostics.selene,
-          null_ls.builtins.formatting.xmlformat
+          null_ls.builtins.formatting.xmlformat,
+          null_ls.builtins.formatting.latexindent
         },
         on_attach = shared_config.on_attach,
         debounce = 150
