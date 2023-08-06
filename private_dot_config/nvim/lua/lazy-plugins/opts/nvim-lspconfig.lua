41c4e1a6d681da77cd69f2e9429486191ad2cb05
Will R <github@astutecat.dev>
2023-08-06 17:02:29 +0200

    chore: some more elixir tweaks

--- private_dot_config/nvim/lua/lazy-plugins/opts/nvim-lspconfig.lua
+++ private_dot_config/nvim/lua/lazy-plugins/opts/nvim-lspconfig.lua
@@ -1,12 +1,13 @@
 local shared_config = require("lazy-plugins.opts.lsp-shared")
 local mason_path = vim.fn.stdpath('data') .. "/mason/bin/"
+local lspconfig = require('lspconfig')
 
 return {
   -- The first entry (without a key) will be the default handler
   -- and will be called for each installed server that doesn't have
   -- a dedicated handler.
   function(server_name) -- default handler (optional)
-    require("lspconfig")[server_name].setup {
+    lspconfig[server_name].setup {
       on_attach = shared_config.on_attach,
       capabilities = shared_config.capabilities,
       flags = {
@@ -18,7 +19,7 @@ return {
     local beam_utils = require("beam_utils")
     local flag = require("config_flags")
     if flag.on_d9 and beam_utils.erl_version() < 22 then return end
-    require("lspconfig")["erlangls"].setup {
+    lspconfig["erlangls"].setup {
       on_attach = shared_config.on_attach,
       capabilities = shared_config.capabilities,
       flags = {
@@ -30,7 +31,7 @@ return {
   ["elixirls"] = function()
     local has_elixir, _ = pcall(require, "elixir")
     if not has_elixir then
-      require 'lspconfig'.elixirls.setup {
+      lspconfig.elixirls.setup {
         on_attach = shared_config.on_attach,
         capabilities = shared_config.capabilities,
         flags = { debounce_text_changes = 150 },
@@ -41,7 +42,7 @@ return {
   -- Next, you can provide a dedicated handler for specific servers.
   -- For example, a handler override for the `rust_analyzer`:
   ["lua_ls"] = function()
-    require 'lspconfig'.lua_ls.setup {
+    lspconfig.lua_ls.setup {
       on_attach = shared_config.on_attach,
       capabilities = shared_config.capabilities,
       flags = { debounce_text_changes = 150 },
@@ -60,7 +61,7 @@ return {
     }
   end,
   ["yamlls"] = function()
-    require 'lspconfig'.yamlls.setup {
+    lspconfig.yamlls.setup {
       on_attach = shared_config.on_attach,
       capabilities = shared_config.capabilities,
       flags = { debounce_text_changes = 150 },
@@ -72,9 +73,10 @@ return {
     }
   end,
   ["tailwindcss"] = function()
-    require 'lspconfig'.tailwindcss.setup({
+    lspconfig.tailwindcss.setup({
       capabilities = shared_config.capabilities,
       filetypes = { "html", "elixir", "eelixir", "heex" },
+      root_dir = lspconfig.util.root_pattern('tailwind.config.js', 'tailwind.config.ts', 'postcss.config.js', 'postcss.config.ts', 'package.json', 'node_modules', '.git', 'mix.exs'),
       init_options = {
         userLanguages = {
           elixir = "html-eex",
@@ -94,7 +96,7 @@ return {
     })
   end,
   ["emmet_ls"] = function()
-    require 'lspconfig'.emmet_ls.setup({
+    lspconfig.emmet_ls.setup({
       capabilities = shared_config.capabilities,
       filetypes = { "html", "css", "elixir", "eelixir", "heex" },
     })
