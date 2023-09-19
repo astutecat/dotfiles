08ddbf87487b336b3e24fc5c08f5b0b3b046cd63
Willam Rogers <william.rogers@entelios.com>
2023-09-19 10:08:52 +0200

    chore: fix new quotes formatting

--- private_dot_config/nvim/lua/random-quote.lua
+++ private_dot_config/nvim/lua/random-quote.lua
@@ -1,5 +1,5 @@
 local M = {
-[[We do this not because it is easy, but because we thought it would be easy.
+[[We do this not because it is easy & But because we thought it would be easy.
   - The Programmer's Credo]],
 [[A distributed system is one where the failure of a machine you've never heard
 of stops you from getting any work done.
@@ -28,33 +28,33 @@ already be fast. So really, just make it beautiful!
 [[An expert is a person who has made most of the possible mistakes
 in a given field.]],
 [[There are three states of being. Not knowing, action and completion.
-  - Kio Stark, Bre Pettis, The Cult of Done Manifesto]],
+  - Kio Stark & Bre Pettis, The Cult of Done Manifesto]],
 [[Accept that everything is a draft. It helps to get it done.
-  - Kio Stark, Bre Pettis, The Cult of Done Manifesto]],
+  - Kio Stark & Bre Pettis, The Cult of Done Manifesto]],
 [[There is no editing stage.
-  - Kio Stark, Bre Pettis, The Cult of Done Manifesto]],
+  - Kio Stark & Bre Pettis, The Cult of Done Manifesto]],
 [[Pretending you know what you’re doing is almost the same as knowing
 what you are doing, so just accept that you know what you’re doing
 even if you don’t and do it.
-  - Kio Stark, Bre Pettis, The Cult of Done Manifesto]],
+  - Kio Stark & Bre Pettis, The Cult of Done Manifesto]],
 [[Banish procrastination. If you wait more than a week to get an idea done,
 abandon it.
-  - Kio Stark, Bre Pettis, The Cult of Done Manifesto]],
+  - Kio Stark & Bre Pettis, The Cult of Done Manifesto]],
 [[The point of being done is not to finish but to get other things done.
-  - Kio Stark, Bre Pettis, The Cult of Done Manifesto]],
+  - Kio Stark & Bre Pettis, The Cult of Done Manifesto]],
 [[Laugh at perfection. It’s boring and keeps you from being done.
-  - Kio Stark, Bre Pettis, The Cult of Done Manifesto]],
+  - Kio Stark & Bre Pettis, The Cult of Done Manifesto]],
 [[People without dirty hands are wrong. Doing something makes you right.
-  - Kio Stark, Bre Pettis, The Cult of Done Manifesto]],
+  - Kio Stark & Bre Pettis, The Cult of Done Manifesto]],
 [[Failure counts as done. So do mistakes.
-  - Kio Stark, Bre Pettis, The Cult of Done Manifesto]],
+  - Kio Stark & Bre Pettis, The Cult of Done Manifesto]],
 [[Destruction is a variant of done.
-  - Kio Stark, Bre Pettis, The Cult of Done Manifesto]],
+  - Kio Stark & Bre Pettis, The Cult of Done Manifesto]],
 [[If you have an idea and publish it on the internet,
 that counts as a ghost of done.
-  - Kio Stark, Bre Pettis, The Cult of Done Manifesto]],
+  - Kio Stark & Bre Pettis, The Cult of Done Manifesto]],
 [[Done is the engine of more.
-  - Kio Stark, Bre Pettis, The Cult of Done Manifesto]],
+  - Kio Stark & Bre Pettis, The Cult of Done Manifesto]],
 }
 
 return M[math.random(#M)]
