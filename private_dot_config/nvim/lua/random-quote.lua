8cb4796ea09ce6e1beec1cfae346a0714b5a45fc
Willam Rogers <william.rogers@entelios.com>
2024-04-24 11:51:48 +0200

    fix: incorrect formatting on a quote

--- private_dot_config/nvim/lua/random-quote.lua
+++ private_dot_config/nvim/lua/random-quote.lua
@@ -1,5 +1,6 @@
 local M = {
-[[We do this not because it is easy & But because we thought it would be easy.
+[[We do this not because it is easy,
+but because we thought it would be easy.
   - The Programmer's Credo]],
 [[A distributed system is one where the failure of a machine you've never heard
 of stops you from getting any work done.
