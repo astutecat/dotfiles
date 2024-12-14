b2831c474e93e71ad2afef7c3bb0c28e15a9dfdc
Will R <github@astutecat.dev>
2024-12-15 00:22:20 +0100

    chore: add neogit

--- private_dot_config/nvim/lua/lazy-plugins/git.lua
+++ private_dot_config/nvim/lua/lazy-plugins/git.lua
@@ -9,67 +9,64 @@ local gitsigns_config = {
     end
 
     -- Navigation
-    map('n', ']c', function()
-      if vim.wo.diff then return ']c' end
-      vim.schedule(function() gs.next_hunk() end)
-      return '<Ignore>'
+    map("n", "]c", function()
+      if vim.wo.diff then
+        return "]c"
+      end
+      vim.schedule(function()
+        gs.next_hunk()
+      end)
+      return "<Ignore>"
     end, { expr = true })
 
-    map('n', '[c', function()
-      if vim.wo.diff then return '[c' end
-      vim.schedule(function() gs.prev_hunk() end)
-      return '<Ignore>'
+    map("n", "[c", function()
+      if vim.wo.diff then
+        return "[c"
+      end
+      vim.schedule(function()
+        gs.prev_hunk()
+      end)
+      return "<Ignore>"
     end, { expr = true })
 
     --TODO: Refactor this to use legendary.nvim
 
     -- Actions
-    map({ 'n', 'v' }, '<leader>hs', ':Gitsigns stage_hunk<CR>')
-    map({ 'n', 'v' }, '<leader>hr', ':Gitsigns reset_hunk<CR>')
-    map('n', '<leader>hS', ':Gitsigns stage_buffer<cr>')
-    map('n', '<leader>hu', ':Gitsigns undo_stage_hunk<cr>')
-    map('n', '<leader>hR', ':Gitsigns reset_buffer<cr>')
-    map('n', '<leader>hp', ':Gitsigns preview_hunk<cr>')
-    map('n', '<leader>tb', ':Gitsigns toggle_current_line_blame<cr>')
-    map('n', '<leader>hd', ':Gitsigns diffthis<cr>')
-    map('n', '<leader>td', ':Gitsigns toggle_deleted<cr>')
+    map({ "n", "v" }, "<leader>hs", ":Gitsigns stage_hunk<CR>")
+    map({ "n", "v" }, "<leader>hr", ":Gitsigns reset_hunk<CR>")
+    map("n", "<leader>hS", ":Gitsigns stage_buffer<cr>")
+    map("n", "<leader>hu", ":Gitsigns undo_stage_hunk<cr>")
+    map("n", "<leader>hR", ":Gitsigns reset_buffer<cr>")
+    map("n", "<leader>hp", ":Gitsigns preview_hunk<cr>")
+    map("n", "<leader>tb", ":Gitsigns toggle_current_line_blame<cr>")
+    map("n", "<leader>hd", ":Gitsigns diffthis<cr>")
+    map("n", "<leader>td", ":Gitsigns toggle_deleted<cr>")
 
     -- Text object
-    map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
+    map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>")
   end,
   update_debounce = 100,
-  attach_to_untracked = false
+  attach_to_untracked = false,
 }
 
 return {
   {
-    "kdheepak/lazygit.nvim",
-    -- optional for floating window border decoration
+    "NeogitOrg/neogit",
     dependencies = {
-      "nvim-lua/plenary.nvim",
+      "nvim-lua/plenary.nvim",         -- required
+      "sindrets/diffview.nvim",        -- optional - Diff integration
+      -- Only one of these is needed.
+      "nvim-telescope/telescope.nvim", -- optional
     },
-    opts = {},
-    config = function(_, _)
-      local mappings = {
-        {
-          "<leader>gg",
-          "<cmd>LazyGit<cr>",
-          mode = { 'n' },
-          description = "LazyGit"
-        },
-      }
-      require('legendary').keymaps(mappings)
-    end
+    config = true,
   },
-
   {
-    'lewis6991/gitsigns.nvim',
+    "lewis6991/gitsigns.nvim",
     opts = gitsigns_config,
   },
 
   {
     "FabijanZulj/blame.nvim",
-    opts = {}
-  }
-
+    opts = {},
+  },
 }
