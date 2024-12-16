e34a8c00854b7d19996b92be2dd16d90909175b7
Willam Rogers <william.rogers@entelios.com>
2024-12-16 17:38:19 +0100

    feat: wezterm config

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -1,7 +1,13 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 -- config.color_scheme = 'AdventureTime'
-config.color_scheme = 'Tokyo Night'
-config.font = wezterm.font 'Iosevka Nerd Font'
+config.color_scheme = "Tokyo Night"
+config.font = wezterm.font_with_fallback({
+  "MonoLisa",
+  "Fira Code Nerd Font",
+  "Iosevka Nerd Font",
+  "Fira Code",
+  "Iosevka",
+})
 config.hide_tab_bar_if_only_one_tab = true
 return config
