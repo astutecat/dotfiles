f1eb400f1ed5b3d73c58d6f619a24dfa1853bb23
Willam Rogers <william.rogers@entelios.com>
2024-12-16 12:11:51 +0100

    chore: hide tab bar

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -3,4 +3,5 @@ local config = wezterm.config_builder()
 -- config.color_scheme = 'AdventureTime'
 config.color_scheme = 'Tokyo Night'
 config.font = wezterm.font 'Iosevka Nerd Font'
+config.hide_tab_bar_if_only_one_tab = true
 return config
