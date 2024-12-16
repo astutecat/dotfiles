2030233ed1476ab5f26b64b836bbae9699844419
Willam Rogers <william.rogers@entelios.com>
2024-12-16 12:10:51 +0100

    chore: add more fonts

---
+++ dot_wezterm.lua
@@ -0,0 +1,6 @@
+local wezterm = require 'wezterm'
+local config = wezterm.config_builder()
+-- config.color_scheme = 'AdventureTime'
+config.color_scheme = 'Tokyo Night'
+config.font = wezterm.font 'Iosevka Nerd Font'
+return config
