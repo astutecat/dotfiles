4893ddb391ffd62c76d0f55dda1bf0424ff7d56b
Will R <github@astutecat.dev>
2025-01-04 10:59:55 +0100

    chore: add recommended library for test integration

--- private_dot_config/nvim/lua/lazy-plugins/test-integration.lua
+++ private_dot_config/nvim/lua/lazy-plugins/test-integration.lua
@@ -4,6 +4,7 @@ return {
     dependencies = {
       "nvim-neotest/nvim-nio",
       "nvim-lua/plenary.nvim",
+      "antoinemadec/FixCursorHold.nvim",
       "nvim-treesitter/nvim-treesitter",
       "vim-test/vim-test",
       "jfpedroza/neotest-elixir",
