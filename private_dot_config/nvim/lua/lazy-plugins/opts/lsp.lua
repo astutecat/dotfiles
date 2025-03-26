393ee192526a5954a058f5125a4562508ddbd89e
Willam Rogers <william.rogers@entelios.com>
2025-03-26 15:16:15 +0100

    fix: typos_lsp

--- private_dot_config/nvim/lua/lazy-plugins/opts/lsp.lua
+++ private_dot_config/nvim/lua/lazy-plugins/opts/lsp.lua
@@ -145,7 +145,7 @@ return {
         -- Custom config. Used together with a config file found in the workspace or its parents,
         -- taking precedence for settings declared in both.
         -- Equivalent to the typos `--config` cli argument.
-        config = '~/.config/typos.toml',
+        -- config = '~/.config/typos.toml',
         -- How typos are rendered in the editor, can be one of an Error, Warning, Info or Hint.
         -- Defaults to error.
         diagnosticSeverity = "Hint"
