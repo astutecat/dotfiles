9e419e9b665a0801341ba34b7a789f11255de21e
Will R <github@astutecat.dev>
2025-08-10 10:38:37 +0200

    chore: move completion generation to chezmoi to speed up zsh start

---
+++ run_after_generate_completions.zsh
@@ -0,0 +1,37 @@
+#!/bin/zsh
+
+function is_cmd() {
+  [[ -n $(command -v $1) ]]
+}
+
+if is_cmd cog; then
+  cog generate-completions zsh >! "${ZDOTDIR:-$HOME}/.zfunc/_cog"
+fi
+
+if is_cmd rustup; then
+  rustup completions zsh >! "${ZDOTDIR:-$HOME}/.zfunc/_rustup"
+fi
+
+if is_cmd just; then
+  just --completions zsh >! "${ZDOTDIR:-$HOME}/.zfunc/_just"
+fi
+
+if is_cmd atuin; then
+  atuin gen-completions --shell zsh >! "${ZDOTDIR:-$HOME}/.zfunc/_atuin"
+fi
+
+if is_cmd stg; then
+  stg completion zsh >! "${ZDOTDIR:-$HOME}/.zfunc/_stg"
+fi
+
+if is_cmd devox; then
+  devbox completion zsh >! "${ZDOTDIR:-$HOME}/.zfunc/_devbox"
+fi
+
+if is_cmd restic; then
+  restic generate --zsh-completion "${ZDOTDIR:-$HOME}/.zfunc/_restic" > /dev/null
+fi
+
+if is_cmd zellij; then
+  zellij setup --generate-completion zsh >! "${ZDOTDIR:-$HOME}/.zfunc/_zellij"
+fi
