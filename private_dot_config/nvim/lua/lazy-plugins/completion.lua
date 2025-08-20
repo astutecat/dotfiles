3945f6ec2e3bc63964c08cd7577a6ebb41225f5f
Will Rogers <william.rogers@entelios.com>
2025-08-20 10:20:52 +0200

    feat: adjust completion config

--- private_dot_config/nvim/lua/lazy-plugins/completion.lua
+++ private_dot_config/nvim/lua/lazy-plugins/completion.lua
@@ -20,7 +20,7 @@ return {
       -- C-k: Toggle signature help (if signature.enabled = true)
       --
       -- See :h blink-cmp-config-keymap for defining your own keymap
-      keymap = { preset = 'default' },
+      keymap = { preset = 'enter' },
 
       appearance = {
         -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
