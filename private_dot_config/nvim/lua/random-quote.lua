6278545f15c6665cddeeaa731968e081d3e36c29
Will Rogers <william.rogers@entelios.com>
2025-11-17 09:15:34 +0100

    chore: add quote

--- private_dot_config/nvim/lua/random-quote.lua
+++ private_dot_config/nvim/lua/random-quote.lua
@@ -1,66 +1,68 @@
 local M = {
-[[We do this not because it is easy,
+  [[We do this not because it is easy,
 but because we thought it would be easy.
   - The Programmer's Credo]],
-[[A distributed system is one where the failure of a machine you've never heard
+  [[A distributed system is one where the failure of a machine you've never heard
 of stops you from getting any work done.
   - Leslie Lamport]],
-[[Hope is not a strategy.
+  [[Hope is not a strategy.
   - Site reliability engineers' motto.]],
-[[If it's a good idea, go ahead and do it. It's much easier to apologize than
+  [[If it's a good idea, go ahead and do it. It's much easier to apologize than
 it is to get permission.
   - Grace Hopper]],
-[[It's not DNS
+  [[It's not DNS
 There's no way it's DNS
 It was DNS
   - SSBroski]],
-[[       A COMPUTER
+  [[       A COMPUTER
 CAN NEVER BE HELD ACCOUNTABLE
 
 THEREFORE A COMPUTER MUST NEVER
   MAKE A MANAGEMENT DECISION
   - IBM Presentation Slide, ca. 1970s]],
-[[Make it work, then make it beautiful, then if you really, really have to,
+  [[Make it work, then make it beautiful, then if you really, really have to,
 make it fast. 90 percent of the time, if you make it beautiful, it will
 already be fast. So really, just make it beautiful!
   - Joe Armstrong]],
-[[Failure free operations require experience with failure.
+  [[Failure free operations require experience with failure.
   - Richard I. Cook, MD. 'How Complex Systems Fail'.]],
-[[An expert is a person who has made most of the possible mistakes
+  [[An expert is a person who has made most of the possible mistakes
 in a given field.]],
-[[There are three states of being. Not knowing, action and completion.
+  [[There are three states of being. Not knowing, action and completion.
   - Kio Stark & Bre Pettis, The Cult of Done Manifesto]],
-[[Accept that everything is a draft. It helps to get it done.
+  [[Accept that everything is a draft. It helps to get it done.
   - Kio Stark & Bre Pettis, The Cult of Done Manifesto]],
-[[There is no editing stage.
+  [[There is no editing stage.
   - Kio Stark & Bre Pettis, The Cult of Done Manifesto]],
-[[Pretending you know what you’re doing is almost the same as knowing
+  [[Pretending you know what you’re doing is almost the same as knowing
 what you are doing, so just accept that you know what you’re doing
 even if you don’t and do it.
   - Kio Stark & Bre Pettis, The Cult of Done Manifesto]],
-[[Banish procrastination. If you wait more than a week to get an idea done,
+  [[Banish procrastination. If you wait more than a week to get an idea done,
 abandon it.
   - Kio Stark & Bre Pettis, The Cult of Done Manifesto]],
-[[The point of being done is not to finish but to get other things done.
+  [[The point of being done is not to finish but to get other things done.
   - Kio Stark & Bre Pettis, The Cult of Done Manifesto]],
-[[Laugh at perfection. It’s boring and keeps you from being done.
+  [[Laugh at perfection. It’s boring and keeps you from being done.
   - Kio Stark & Bre Pettis, The Cult of Done Manifesto]],
-[[People without dirty hands are wrong. Doing something makes you right.
+  [[People without dirty hands are wrong. Doing something makes you right.
   - Kio Stark & Bre Pettis, The Cult of Done Manifesto]],
-[[Failure counts as done. So do mistakes.
+  [[Failure counts as done. So do mistakes.
   - Kio Stark & Bre Pettis, The Cult of Done Manifesto]],
-[[Destruction is a variant of done.
+  [[Destruction is a variant of done.
   - Kio Stark & Bre Pettis, The Cult of Done Manifesto]],
-[[If you have an idea and publish it on the internet,
+  [[If you have an idea and publish it on the internet,
 that counts as a ghost of done.
   - Kio Stark & Bre Pettis, The Cult of Done Manifesto]],
-[[Done is the engine of more.
+  [[Done is the engine of more.
   - Kio Stark & Bre Pettis, The Cult of Done Manifesto]],
-[[Software development is best seen as a learning process where running programs
+  [[Software development is best seen as a learning process where running programs
 are the proof of learning, rather than a factory or production process.
 - Kent Beck]],
-[[Everyone has a plan until they get punched in the face.
+  [[Everyone has a plan until they get punched in the face.
 - Mike Tyson]],
+  [[Don’t cling to a mistake just because you spent a lot of time making it.
+- Aubrey de Grey]],
 }
 
 return M[math.random(#M)]
