91aee1befbd1b86462be23671941f92db3ff9c47
Will Rogers <william.rogers@entelios.com>
2025-12-01 09:25:52 +0100

    chore: add some more prompt settings

--- private_dot_config/private_fish/conf.d/prompt.fish
+++ private_dot_config/private_fish/conf.d/prompt.fish
@@ -1,1 +1,4 @@
 set --universal pure_symbol_prompt λ
+set --universal pure_show_prefix_root_prompt true
+set --universal pure_enable_nixdevshell true
+set --universal pure_symbol_title_bar_separator :
