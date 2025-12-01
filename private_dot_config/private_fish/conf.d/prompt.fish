969ec2c6aba595c37973b3fb0225df3ab8cb611c
Will Rogers <william.rogers@entelios.com>
2025-12-01 10:22:54 +0100

    chore: adjust prompt settings a bit more

--- private_dot_config/private_fish/conf.d/prompt.fish
+++ private_dot_config/private_fish/conf.d/prompt.fish
@@ -2,3 +2,5 @@ set --universal pure_symbol_prompt λ
 set --universal pure_show_prefix_root_prompt true
 set --universal pure_enable_nixdevshell true
 set --universal pure_symbol_title_bar_separator :
+set --universal pure_show_numbered_git_indicator true
+set --universal pure_color_primary cyan
