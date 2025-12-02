568379602f499c77b62b9e3a5d3922cfb579df78
Will Rogers <github@astutecat.dev>
2025-12-02 11:19:22 +0100

    chore(nvim): adjust neo-tree settings

--- private_dot_config/nvim/lua/lazy-plugins/tree.lua
+++ private_dot_config/nvim/lua/lazy-plugins/tree.lua
@@ -22,7 +22,7 @@ return {
       window = {
         width = 30,
         mappings = {
-          ['Y'] = function(state)
+          ["Y"] = function(state)
             local node = state.tree:get_node()
             local filepath = node:get_id()
             local filename = node.name
@@ -30,33 +30,34 @@ return {
 
             local results = {
               filepath,
-              modify(filepath, ':.'),
-              modify(filepath, ':~'),
+              modify(filepath, ":."),
+              modify(filepath, ":~"),
               filename,
-              modify(filename, ':r'),
-              modify(filename, ':e'),
+              modify(filename, ":r"),
+              modify(filename, ":e"),
             }
 
             vim.ui.select({
-              '1. Absolute path: ' .. results[1],
-              '2. Path relative to CWD: ' .. results[2],
-              '3. Path relative to HOME: ' .. results[3],
-              '4. Filename: ' .. results[4],
-              '5. Filename without extension: ' .. results[5],
-              '6. Extension of the filename: ' .. results[6],
-            }, { prompt = 'Choose to copy to clipboard:' }, function(choice)
+              "1. Absolute path: " .. results[1],
+              "2. Path relative to CWD: " .. results[2],
+              "3. Path relative to HOME: " .. results[3],
+              "4. Filename: " .. results[4],
+              "5. Filename without extension: " .. results[5],
+              "6. Extension of the filename: " .. results[6],
+            }, { prompt = "Choose to copy to clipboard:" }, function(choice)
               local i = tonumber(choice:sub(1, 1))
               local result = results[i]
               vim.fn.setreg('"', result)
-              vim.notify('Copied: ' .. result)
+              vim.notify("Copied: " .. result)
             end)
-          end
-        }
+          end,
+        },
       },
       filesystem = {
         group_empty_dirs = true,
         use_libuv_file_watcher = true,
         filtered_items = {
+          hide_gitignored = false,
           always_show = {
             ".gitignore",
             ".envrc",
@@ -73,12 +74,12 @@ return {
             ".editorconfig",
             ".chezmoitemplates",
             ".ignore",
-            ".github"
+            ".github",
           },
-        }
+        },
       },
       buffers = {
-        group_empty_dirs = true
+        group_empty_dirs = false,
       },
       document_symbols = {
         kinds = {
@@ -97,39 +98,39 @@ return {
           Struct = { icon = "󰌗", hl = "Type" },
           Operator = { icon = "󰆕", hl = "Operator" },
           TypeParameter = { icon = "󰊄", hl = "Type" },
-          StaticMethod = { icon = '󰠄 ', hl = 'Function' },
-        }
+          StaticMethod = { icon = "󰠄 ", hl = "Function" },
+        },
       },
       default_component_configs = {
         icon = {
           folder_empty = "󰜌",
           folder_empty_open = "󰜌",
-          default = '',
+          default = "",
         },
         git_status = {
           symbols = {
-            renamed  = "󰁕",
+            renamed = "󰁕",
             unstaged = "󰄱",
           },
         },
         modified = {
-          symbol = '',
-        }
+          symbol = "",
+        },
       },
       event_handlers = {
         {
           event = "neo_tree_window_after_open",
           handler = function(_)
-            pcall(vim.cmd [[wincmd =]])
-          end
+            pcall(vim.cmd([[wincmd =]]))
+          end,
         },
         {
           event = "neo_tree_window_after_close",
           handler = function(_)
-            pcall(vim.cmd [[wincmd =]])
-          end
-        }
-      }
+            pcall(vim.cmd([[wincmd =]]))
+          end,
+        },
+      },
     },
     config = function(_, opts)
       vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
@@ -138,33 +139,32 @@ return {
         {
           "<leader>ee",
           "<cmd>Neotree toggle<cr>",
-          mode = { 'n' },
-          description = "Neotree: Toggle (left split)"
+          mode = { "n" },
+          description = "Neotree: Toggle (left split)",
         },
         {
           "<leader>ew",
           "<cmd>Neotree toggle position=current<cr>",
-          mode = { 'n' },
-          description = "Neotree: Toggle netrw style"
+          mode = { "n" },
+          description = "Neotree: Toggle netrw style",
         },
         {
           "<leader>err",
           "<cmd>Neotree reveal<cr>",
-          mode = { 'n' },
-          description = "Neotree: Reveal File"
+          mode = { "n" },
+          description = "Neotree: Reveal File",
         },
         {
           "<leader>erw",
           "<cmd>Neotree reveal position=current<cr>",
-          mode = { 'n' },
-          description = "Neotree: Reveal File netrw style"
-        }
+          mode = { "n" },
+          description = "Neotree: Reveal File netrw style",
+        },
       }
-      require('legendary').keymaps(mappings)
+      require("legendary").keymaps(mappings)
       local wk = require("which-key")
       wk.add({ "<Leader>e", group = "explore" })
       require("neo-tree").setup(opts)
-    end
+    end,
   },
-
 }
