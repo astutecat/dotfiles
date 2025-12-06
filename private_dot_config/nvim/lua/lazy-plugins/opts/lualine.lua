cda05dd2168ebf38dfa4e9d2aec1609b90bdab46
Will Rogers <github@astutecat.dev>
2025-12-06 08:57:28 +0100

    chore: adjust nvim diagnostic icons

--- private_dot_config/nvim/lua/lazy-plugins/opts/lualine.lua
+++ private_dot_config/nvim/lua/lazy-plugins/opts/lualine.lua
@@ -13,18 +13,18 @@ local function buffer_width()
 end
 
 local function custom_location()
-  local line = vim.fn.line('.')
-  local col = vim.fn.virtcol('.')
-  local total = vim.fn.line('$')
-  return string.format('%3d:%-2d/%-d', line, col, total)
+  local line = vim.fn.line(".")
+  local col = vim.fn.virtcol(".")
+  local total = vim.fn.line("$")
+  return string.format("%3d:%-2d/%-d", line, col, total)
 end
 
 return {
   options = {
     icons_enabled = true,
-    theme = 'auto',
-    component_separators = { left = '', right = '' },
-    section_separators = { left = '', right = '' },
+    theme = "auto",
+    component_separators = { left = "", right = "" },
+    section_separators = { left = "", right = "" },
     disabled_filetypes = {
       statusline = {},
       winbar = {},
@@ -36,17 +36,29 @@ return {
       statusline = 1000,
       tabline = 1000,
       winbar = 1000,
-    }
+    },
   },
   sections = {
-    lualine_a = { 'mode' },
-    lualine_b = { 'branch', 'diff', 'diagnostics' },
-    lualine_c = { 'filename' },
+    lualine_a = { "mode" },
+    lualine_b = {
+      "branch",
+      "diff",
+      {
+        "diagnostics",
+        symbols = {
+          error = "󰅙",
+          info = "󰋼",
+          hint = "󰌵",
+          warn = "",
+        },
+      },
+    },
+    lualine_c = { "filename" },
     lualine_x = {
-      'lsp_status',
-      'encoding',
+      "lsp_status",
+      "encoding",
       -- 'fileformat',
-      'filetype',
+      "filetype",
       {
         require("lazy.status").updates,
         cond = require("lazy.status").has_updates,
@@ -54,57 +66,53 @@ return {
       },
     },
     lualine_y = {
-      { 'progress' }
+      { "progress" },
     },
     lualine_z = {
-      { custom_location }
-    }
+      { custom_location },
+    },
   },
   inactive_sections = {
     lualine_a = {},
     lualine_b = {},
-    lualine_c = { 'filename' },
+    lualine_c = { "filename" },
     lualine_x = {
-      { custom_location }
+      { custom_location },
     },
     lualine_y = {},
-    lualine_z = {}
+    lualine_z = {},
   },
   tabline = {
     lualine_a = {
       {
-        'buffers',
+        "buffers",
         show_filename_only = false,
         use_mode_colors = true,
         max_length = buffer_width,
-        separator = ' ',
+        separator = " ",
         symbols = {
-          modified = ' ●',              -- Text to show when the buffer is modified
-          alternate_file = '󰐤 ',         -- Text to show to identify the alternate file
-          directory = ' ',             -- Text to show when the buffer is a directory
+          modified = " ●", -- Text to show when the buffer is modified
+          alternate_file = "󰐤 ", -- Text to show to identify the alternate file
+          directory = " ", -- Text to show when the buffer is a directory
         },
-      }
+      },
     },
-    lualine_b = {
-    },
-    lualine_c = {
-    },
-    lualine_x = {
-    },
+    lualine_b = {},
+    lualine_c = {},
+    lualine_x = {},
     lualine_z = {
       {
-        'tabs',
+        "tabs",
         mode = 2,
         use_mode_colors = true,
         tab_max_length = 20,
-        max_length = tab_width
-      }
+        max_length = tab_width,
+      },
     },
   },
   winbar = {},
-  inactive_winbar = {
-  },
+  inactive_winbar = {},
   extensions = {
-    'neo-tree'
-  }
+    "neo-tree",
+  },
 }
