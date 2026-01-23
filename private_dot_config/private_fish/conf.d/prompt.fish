327911c687bd76a8bdf1b6249c9f6afc6259814d
Will Rogers <william.rogers@entelios.com>
2026-01-23 15:36:58 +0100

    feat(fish): switch to hydro and add jj support

--- private_dot_config/private_fish/conf.d/prompt.fish
+++ private_dot_config/private_fish/conf.d/prompt.fish
@@ -7,6 +7,8 @@
 # set --universal pure_color_success FF4F00
 # set --universal fish_transient_prompt 1
 # set --universal pure_enable_single_line_prompt false
+# set --universal pure_show_exit_status true
 
 set --universal hydro_symbol_prompt λ
 set --universal hydro_color_prompt FF4F00
+set --universal hydro_multiline true
