5c91b8513b7ca2e03dd7d9f2ded59a7fb46d2a84
Will Rogers <william.rogers@entelios.com>
2026-02-03 09:29:33 +0100

    chore: add ability to toggle format on save

--- private_dot_config/nvim/lua/lazy-plugins/lsp.lua
+++ private_dot_config/nvim/lua/lazy-plugins/lsp.lua
@@ -139,7 +139,13 @@ local function conform_opts()
       lsp_format = "fallback",
     },
     -- Set up format-on-save
-    format_on_save = { timeout_ms = 500 },
+    format_on_save = function(bufnr)
+      -- Disable with a global or buffer-local variable
+      if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+        return
+      end
+      return { timeout_ms = 500, lsp_format = "fallback" }
+    end,
     -- Customize formatters
     formatters = {
       shfmt = {
@@ -169,6 +175,24 @@ return {
     init = function()
       -- If you want the formatexpr, here is the place to set it
       vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+
+      vim.api.nvim_create_user_command("FormatDisable", function(args)
+        if args.bang then
+          -- FormatDisable! will disable formatting just for this buffer
+          vim.b.disable_autoformat = true
+        else
+          vim.g.disable_autoformat = true
+        end
+      end, {
+        desc = "Disable autoformat-on-save",
+        bang = true,
+      })
+      vim.api.nvim_create_user_command("FormatEnable", function()
+        vim.b.disable_autoformat = false
+        vim.g.disable_autoformat = false
+      end, {
+        desc = "Re-enable autoformat-on-save",
+      })
     end,
   },
   {
