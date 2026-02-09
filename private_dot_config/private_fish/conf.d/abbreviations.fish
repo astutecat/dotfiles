9a43f80cb6ef605b2aa0c2e43486c216cc6b3141
Will Rogers <william.rogers@entelios.com>
2026-02-09 13:45:29 +0100

    chore: add some more jj tweaks (well, remove them)

--- private_dot_config/private_fish/conf.d/abbreviations.fish
+++ private_dot_config/private_fish/conf.d/abbreviations.fish
@@ -33,7 +33,6 @@ abbr -a --command jj desc describe
 abbr -a --command jj r redo
 abbr -a --command jj u undo
 abbr -a --command jj g git
-abbr -a --command jj l log
 abbr -a --command={jj,git,hg} st status
 
 function last_history_item
