67a8b0a4aa51386f8682e3f9c833953f13b30d39
Will Rogers <github@astutecat.dev>
2026-03-17 11:35:00 +0100

    chore: remove jj completions generation

--- private_dot_config/private_fish/functions/update_fish_completions.fish
+++ private_dot_config/private_fish/functions/update_fish_completions.fish
@@ -1,46 +1,45 @@
 function update_fish_completions
-  echo "updating fish completions..."
-  set --local completions_dir ~/.config/fish/completions
+    echo "updating fish completions..."
+    set --local completions_dir ~/.config/fish/completions
 
-  if type -q mise
-    mise completion fish > $completions_dir/mise.fish
-  end
-
-  if type -q prek
-    begin
-      set -lx COMPLETE fish
-      prek > ~/.config/fish/completions/prek.fish
+    if type -q mise
+        mise completion fish >$completions_dir/mise.fish
     end
-  end
 
-  if type -q rustup
-    rustup completions fish > $completions_dir/rustup.fish
-  end
+    if type -q prek
+        begin
+            set -lx COMPLETE fish
+            prek >~/.config/fish/completions/prek.fish
+        end
+    end
 
-  if type -q atuin
-    atuin gen-completions --shell fish > $completions_dir/atuin.fish
-  end
+    if type -q rustup
+        rustup completions fish >$completions_dir/rustup.fish
+    end
 
-  if type -q stg
-    stg completion fish > $completions_dir/stg.fish
-  end
+    if type -q atuin
+        atuin gen-completions --shell fish >$completions_dir/atuin.fish
+    end
 
-  if type -q devbox
-    devbox completion fish > $completions_dir/devbox.fish
-  end
+    if type -q stg
+        stg completion fish >$completions_dir/stg.fish
+    end
 
-  if type -q restic
-    restic generate --fish-completion $completions_dir/restic.fish
-  end
+    if type -q devbox
+        devbox completion fish >$completions_dir/devbox.fish
+    end
 
-  if type -q trivy
-    trivy completion fish > $completions_dir/trivy.fish
-  end
+    if type -q restic
+        restic generate --fish-completion $completions_dir/restic.fish
+    end
 
-  if type -q jj
-    jj util completion fish > $completions_dir/jj.fish
-    jj util install-man-pages ~/.local/share/man
-  end
+    if type -q trivy
+        trivy completion fish >$completions_dir/trivy.fish
+    end
 
-  echo ...done
+    if type -q jj
+        jj util install-man-pages ~/.local/share/man
+    end
+
+    echo ...done
 end
