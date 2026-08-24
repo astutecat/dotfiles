3367ff61f472853fe9d69e1fe0eee2be2b369845
Will Rogers <github@astutecat.dev>
2026-08-24 10:34:23 +0200

    Revert "chore: use flake-utils to avoid hardcoding system"
    This reverts commit b00e2252ff12e45bde1d6d9ec3109280c4fac755.

--- flake.nix
+++ flake.nix
@@ -5,7 +5,6 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
     home-manager.url = "github:nix-community/home-manager/release-26.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs =
@@ -13,55 +12,54 @@
       self,
       nixpkgs,
       home-manager,
-      flake-utils,
     }:
-    flake-utils.lib.eachDefaultSystemPassThrough (
-      system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-          config.allowUnfree = true;
+    {
+      homeConfigurations = {
+        "willrog@nb0408" = home-manager.lib.homeManagerConfiguration {
+          pkgs = import nixpkgs {
+            system = "x86_64-linux";
+            config.allowUnfree = true;
+          };
+          extraSpecialArgs = {
+            inherit inputs;
+            username = "willrog";
+            hostname = "nb0408";
+            homeDirectory = "/home/willrog";
+          };
+          modules = [
+            ./hosts/nb0408
+          ];
         };
-      in
-      {
-        homeConfigurations = {
-          "willrog@nb0408" = home-manager.lib.homeManagerConfiguration {
-            inherit pkgs;
-            extraSpecialArgs = {
-              inherit inputs;
-              username = "willrog";
-              hostname = "nb0408";
-              homeDirectory = "/home/willrog";
-            };
-            modules = [
-              ./hosts/nb0408
-            ];
+        "astutecat@astutecachy" = home-manager.lib.homeManagerConfiguration {
+          pkgs = import nixpkgs {
+            system = "x86_64-linux";
+            config.allowUnfree = true;
           };
-          "astutecat@astutecachy" = home-manager.lib.homeManagerConfiguration {
-            inherit pkgs;
-            extraSpecialArgs = {
-              inherit inputs;
-              username = "astutecat";
-              hostname = "astutecachy";
-              homeDirectory = "/home/astutecat";
-            };
-            modules = [
-              ./hosts/astutecachy
-            ];
+          extraSpecialArgs = {
+            inherit inputs;
+            username = "astutecat";
+            hostname = "astutecachy";
+            homeDirectory = "/home/astutecat";
           };
-          "astutecat@AstuteMBP" = home-manager.lib.homeManagerConfiguration {
-            inherit pkgs;
-            extraSpecialArgs = {
-              inherit inputs;
-              username = "astutecat";
-              hostname = "AstuteMBP";
-              homeDirectory = "/Users/astutecat";
-            };
-            modules = [
-              ./hosts/AstuteMBP
-            ];
-          };
+          modules = [
+            ./hosts/astutecachy
+          ];
         };
-      }
-    );
+        "astutecat@AstuteMBP" = home-manager.lib.homeManagerConfiguration {
+          pkgs = import nixpkgs {
+            system = "aarch64-darwin";
+            config.allowUnfree = true;
+          };
+          extraSpecialArgs = {
+            inherit inputs;
+            username = "astutecat";
+            hostname = "AstuteMBP";
+            homeDirectory = "/Users/astutecat";
+          };
+          modules = [
+            ./hosts/AstuteMBP
+          ];
+        };
+      };
+    };
 }
