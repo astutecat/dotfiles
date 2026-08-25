1808c9e37aab7ab9281191ea256c8424178d6f6c
Will Rogers <github@astutecat.dev>
2026-08-25 15:07:39 +0200

    chore: cheat config

--- configs/cheat.nix
+++ configs/cheat.nix
@@ -1,5 +1,7 @@
 {
   pkgs,
+  config,
+  lib,
   ...
 }:
 
@@ -30,10 +32,41 @@ let
   };
 in
 {
-  xdg.configFile."cheat/cheatsheets/community" = {
+  # Community cheatsheets (read-only symlink into the Nix store)
+  xdg.dataFile."cheat/cheatsheets/community" = {
     source = "${community-cheatsheets}/share/cheat/cheatsheets";
   };
+
+  # cheat configuration (migrated from chezmoi conf.yml.tmpl)
+  xdg.configFile."cheat/conf.yml".text = ''
+    editor: nvim
+    colorize: true
+    style: onedark
+    formatter: terminal256
+    pager: ${pkgs.most}/bin/most -R
+    cheatpaths:
+      - name: personal
+        path: ${config.xdg.dataHome}/cheat/cheatsheets/personal
+        tags: [personal]
+        readonly: false
+      - name: work
+        path: ${config.xdg.dataHome}/cheat/cheatsheets/work
+        tags: [work]
+        readonly: false
+      - name: community
+        path: ${config.xdg.dataHome}/cheat/cheatsheets/community
+        tags: [community]
+        readonly: true
+  '';
+
+  # Ensure writable cheat directories exist
+  home.activation.createCheatDirs = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+    mkdir -p "${config.xdg.dataHome}/cheat/cheatsheets/personal"
+    mkdir -p "${config.xdg.dataHome}/cheat/cheatsheets/work"
+  '';
+
   home.packages = [
     pkgs.cheat
+    pkgs.most
   ];
 }
