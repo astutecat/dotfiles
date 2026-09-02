c593a3457dea8bbb05ae4a398e60b974087be3f4
Will Rogers <github@astutecat.dev>
2026-09-02 10:20:41 +0200

    chore: update flake + externals

--- editors/imhex.nix
+++ editors/imhex.nix
@@ -6,13 +6,13 @@
 let
   imhex-patterns = pkgs.stdenv.mkDerivation {
     pname = "imhex-patterns";
-    version = "unstable-2026-08-31";
+    version = "unstable-2026-09-01";
 
     src = pkgs.fetchFromGitHub {
       owner = "WerWolv";
       repo = "ImHex-Patterns";
-      rev = "8718f8e935663544d29c4dcbad1da105412394d5";
-      sha256 = "102iy33212xfc9sxijxwl0nr9l465k9h367ii5kbcrcy7h69y4hb";
+      rev = "90739e5a57df0fbe35760bc4b4460fc0abb8dbee";
+      sha256 = "198bs00hcsqvwqmjh8crnbk633xkvb6016d72isfxj12jrd9774p";
     };
 
     # The repo is pure data — no build phase, no dependencies.
