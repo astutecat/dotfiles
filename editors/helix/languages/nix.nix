da32c035d3b8b7e7af5e2699dab99f4ed261319c
Will Rogers <github@astutecat.dev>
2026-09-04 16:45:19 +0200

    feat(helix): add statix to nix

--- editors/helix/languages/nix.nix
+++ editors/helix/languages/nix.nix
@@ -1,15 +1,25 @@
-{ pkgs, ... }: {
+{ pkgs, lib, ... }: {
   programs.helix = {
     extraPackages = with pkgs; [
       nixd
       nixfmt
+      statix
     ];
 
     languages = {
+      language-server.statix = {
+        command = "${lib.getExe pkgs.statix}";
+        args = [ "lsp" ];
+      };
+
       language = [
         {
           name = "nix";
           auto-format = true;
+          language-servers = [
+            "nixd"
+            "statix"
+          ];
         }
       ];
     };
