3502f66d935f213a3189df4c54e373bc2dfa8fa6
Will Rogers <github@astutecat.dev>
2026-09-11 08:34:54 +0200

    chore: incorporate some zed config from another machine

--- editors/zed.nix
+++ editors/zed.nix
@@ -72,6 +72,19 @@
 
     userSettings = {
       format_on_save = "modifications_if_available";
+      cli_default_open_behavior = "existing_window";
+      restore_on_startup = "launchpad";
+      indent_guides.coloring = "indent_aware";
+      toolbar.code_actions = true;
+      which_key.enabled = true;
+      show_edit_predictions = true;
+      telemetry = {
+        diagnostics = false;
+        metrics = false;
+      };
+      git_panel.dock = "left";
+      outline_panel.dock = "left";
+      languages.Erlang.show_edit_predictions = true;
       agent_servers = {
         github-copilot-cli.type = "registry";
         opencode.type = "registry";
@@ -87,7 +100,7 @@
       project_panel.dock = "left";
 
       icon_theme = "Zed (Default)";
-      buffer_font_family = "MonoLisa";
+      buffer_font_family = "Monaspace Argon";
       ui_font_size = 16;
       buffer_font_size = 12;
 
